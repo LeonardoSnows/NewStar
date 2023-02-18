c8a44afef5068fcee3e6e6f93d25e99000ef504b
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-02-18 15:30:38 -0300

    utilizando spring boot no projeto

---
+++ newstar/src/main/java/br/com/newstar/NewstarApplication.java
@@ -0,0 +1,13 @@
+package br.com.newstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstarApplication.class, args);
+	}
+
+}
---
+++ newstar/src/test/java/br/com/newstar/NewstarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.newstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
